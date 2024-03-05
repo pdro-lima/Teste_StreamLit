229faf0511890465f1b9a2083db1de18381f92c0
pdro-lima <162214731+pdro-lima@users.noreply.github.com>
2024-03-05 13:54:16 -0300

    Update StreamLit_Teste_Deploy.py

--- StreamLit_Teste_Deploy.py
+++ StreamLit_Teste_Deploy.py
@@ -18,7 +18,7 @@ with st.container():
     st.title("Participantes do time mais brabo de tecnologia")
     st.image("logo vitrine.jpg", caption="Logo mais bonito de todos os e-commereces de todas as galáxias")
     st.write("Informações dos brabos do time tech")
-    
+    st.text_input("Cole aqui os números dos pedidos: ")
 
 @st.cache_data
 def carregar_tabela():
