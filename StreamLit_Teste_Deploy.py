1686586304505098ce0285b74d1ee6f1ef3afcbc
pdro-lima <162214731+pdro-lima@users.noreply.github.com>
2024-03-05 14:11:46 -0300

    Update StreamLit_Teste_Deploy.py

--- StreamLit_Teste_Deploy.py
+++ StreamLit_Teste_Deploy.py
@@ -18,7 +18,13 @@ with st.container():
     st.title("Participantes do time mais brabo de tecnologia")
     st.image("logo vitrine.jpg", caption="Logo mais bonito de todos os e-commereces de todas as galáxias")
     st.write("Informações dos brabos do time tech")
-    st.text_input("Cole aqui os números dos pedidos: ")
+    
+def coletar_pedidos():
+    input_pedidos = st.text_area("Cole aqui os números dos pedidos: ", height=200)
+    pedidos = input_pedidos.split("/n")
+    st.write("Pedidos Inseridos")
+    for pedido in pedidos:
+        st.write(pedido)
 
 @st.cache_data
 def carregar_tabela():
