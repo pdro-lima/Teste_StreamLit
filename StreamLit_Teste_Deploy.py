d8385d0e2aa598afb5e39c0e952b07060d3dbc5e
pdro-lima <162214731+pdro-lima@users.noreply.github.com>
2024-03-05 12:26:31 -0300

    Add files via upload

---
+++ StreamLit_Teste_Deploy.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+
+# In[8]:
+
+
+import streamlit as st
+import pandas as pd
+
+
+# In[9]:
+
+
+st.set_page_config(page_title="O site do time mais brabo de tecnologia!")
+
+with st.container():
+    st.title("Participantes do time mais brabo de tecnologia")
+    st.image("logo vitrine.jpg", caption="Logo mais bonito de todos os e-commereces de todas as galáxias")
+    st.write("Informações dos brabos do time tech")
+    
+
+@st.cache_data
+def carregar_tabela():
+    tabela = pd.read_csv("tabela.csv")
+    return tabela
+
+
+st.write(carregar_tabela())
+
+
+# In[ ]:
+
+
+
+
